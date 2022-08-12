eb6fe4f8f6b382e23bee16fbed5230f665b2f136
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-08-12 11:19:19 +0900

    creating auto update feature...

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
-const { app, BrowserWindow } = require('electron');
+const { app, autoUpdater, dialog, BrowserWindow } = require("electron");
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+initialize();
+
+function initialize() {
+  if (app.isPackaged) {
+    const server = "https://your-deployment-url.com";
+    const url = `${server}/update/${process.platform}/${app.getVersion()}`;
+
+    autoUpdater.setFeedURL({ url });
+
+    setInterval(() => {
+      autoUpdater.checkForUpdates();
+    }, 60000);
+
+    autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+      const dialogOpts = {
+        type: "info",
+        buttons: ["Restart", "Later"],
+        title: "Application Update",
+        message: process.platform === "win32" ? releaseNotes : releaseName,
+        detail:
+          "A new version has been downloaded. Restart the application to apply the updates."
+      };
+
+      dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        if (returnValue.response === 0) autoUpdater.quitAndInstall();
+      });
+    });
+
+    autoUpdater.on("error", (message) => {
+      console.error("There was a problem updating the application!");
+      console.error(message);
+    });
+  }
+
+  app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+    });
   });
 
-  win.loadFile('index.html');
-};
-
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
     }
   });
-});
+}
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  win.loadFile("index.html");
+}
