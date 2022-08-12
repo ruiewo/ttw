16c64f1c2c5f1bfa3af86991a699bcc046d4db43
twada <twada@liberworks.co.jp>
2022-08-12 11:37:42 +0900

    add electron-updater.

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
 const { app, autoUpdater, dialog, BrowserWindow } = require("electron");
+const { autoUpdater } = require("electron-updater");
 
 initialize();
 
 function initialize() {
-  if (app.isPackaged) {
-    const server = "https://your-deployment-url.com";
-    const url = `${server}/update/${process.platform}/${app.getVersion()}`;
+  autoUpdater.checkForUpdatesAndNotify();
+  // if (app.isPackaged) {
+  //   const server = "https://your-deployment-url.com";
+  //   const url = `${server}/update/${process.platform}/${app.getVersion()}`;
 
-    autoUpdater.setFeedURL({ url });
+  //   autoUpdater.setFeedURL({ url });
 
-    setInterval(() => {
-      autoUpdater.checkForUpdates();
-    }, 60000);
+  //   setInterval(() => {
+  //     autoUpdater.checkForUpdates();
+  //   }, 60000);
 
-    autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
-      const dialogOpts = {
-        type: "info",
-        buttons: ["Restart", "Later"],
-        title: "Application Update",
-        message: process.platform === "win32" ? releaseNotes : releaseName,
-        detail:
-          "A new version has been downloaded. Restart the application to apply the updates."
-      };
+  //   autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+  //     const dialogOpts = {
+  //       type: "info",
+  //       buttons: ["Restart", "Later"],
+  //       title: "Application Update",
+  //       message: process.platform === "win32" ? releaseNotes : releaseName,
+  //       detail:
+  //         "A new version has been downloaded. Restart the application to apply the updates."
+  //     };
 
-      dialog.showMessageBox(dialogOpts).then((returnValue) => {
-        if (returnValue.response === 0) autoUpdater.quitAndInstall();
-      });
-    });
+  //     dialog.showMessageBox(dialogOpts).then((returnValue) => {
+  //       if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  //     });
+  //   });
 
-    autoUpdater.on("error", (message) => {
-      console.error("There was a problem updating the application!");
-      console.error(message);
-    });
-  }
+  //   autoUpdater.on("error", (message) => {
+  //     console.error("There was a problem updating the application!");
+  //     console.error(message);
+  //   });
+  // }
 
   app.whenReady().then(() => {
     createWindow();
