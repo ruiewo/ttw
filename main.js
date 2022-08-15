32e8f9d3d075ae792d664a0fcdc1b5a028cd36aa
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-08-15 15:30:16 +0900

    remove unused logic.

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const { autoUpdater } = require("electron-updater");
 initialize();
 
 function initialize() {
-  // autoUpdater.checkForUpdatesAndNotify();
   autoUpdater.checkForUpdates();
 
   autoUpdater.on("update-downloaded", (info) => {
@@ -27,37 +26,6 @@ function initialize() {
     console.error("There was a problem updating the application!");
     console.error(err);
   });
-
-  // if (app.isPackaged) {
-  //   const server = "https://your-deployment-url.com";
-  //   const url = `${server}/update/${process.platform}/${app.getVersion()}`;
-
-  //   autoUpdater.setFeedURL({ url });
-
-  //   setInterval(() => {
-  //     autoUpdater.checkForUpdates();
-  //   }, 60000);
-
-  //   autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
-  //     const dialogOpts = {
-  //       type: "info",
-  //       buttons: ["Restart", "Later"],
-  //       title: "Application Update",
-  //       message: process.platform === "win32" ? releaseNotes : releaseName,
-  //       detail:
-  //         "A new version has been downloaded. Restart the application to apply the updates."
-  //     };
-
-  //     dialog.showMessageBox(dialogOpts).then((returnValue) => {
-  //       if (returnValue.response === 0) autoUpdater.quitAndInstall();
-  //     });
-  //   });
-
-  //   autoUpdater.on("error", (message) => {
-  //     console.error("There was a problem updating the application!");
-  //     console.error(message);
-  //   });
-  // }
 
   app.whenReady().then(() => {
     createWindow();
